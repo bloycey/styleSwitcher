929508e356a3ea4b0afb99758f7fc31e86964aad
Andrew <bloyce1990@gmail.com>
2018-06-05 22:22:27 +1000

    Added 'session' as an option

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -4,18 +4,22 @@
 
         var settings = $.extend({
             path: "css/customstyles/",
-            prefix: "custom"
+            prefix: "custom",
+            session: true
         }, options);
     
         var path = settings.path;
         var prefix = settings.prefix;
+        var session = settings.session;
     
-        if(sessionStorage.getItem("customStyle") !== null) {
-            $('head').append(sessionStorage.getItem("customStyle"));
-        }
-    
-        if(sessionStorage.getItem("customValue") !== null){
-            $(this).val(sessionStorage.getItem("customValue"));
+        if(session == true) {
+            if(sessionStorage.getItem("customStyle") !== null) {
+                $('head').append(sessionStorage.getItem("customStyle"));
+            }
+        
+            if(sessionStorage.getItem("customValue") !== null){
+                $(this).val(sessionStorage.getItem("customValue"));
+            }
         }
     
         $(this).change(function(){
@@ -29,18 +33,19 @@
                 })
             } else {
 
-            var basicStyle = "<link rel='stylesheet' type='text/css' href='" + path + styleSheet + ".css'>"
-            $('head').append(basicStyle);
-    
-            sessionStorage.setItem("customStyle", basicStyle);
-            sessionStorage.setItem("customValue", styleSheet);
-    
-            $(linkUrl).each(function(){
-                var stylesheetHref = path + styleSheet + ".css";
-                if($(this).attr("href") !== stylesheetHref) {
-                    $(this).remove();
+                var basicStyle = "<link rel='stylesheet' type='text/css' href='" + path + styleSheet + ".css'>"
+                $('head').append(basicStyle);
+        
+                if(session == true){
+                    sessionStorage.setItem("customStyle", basicStyle);
+                    sessionStorage.setItem("customValue", styleSheet);
                 }
-            })
+                $(linkUrl).each(function(){
+                    var stylesheetHref = path + styleSheet + ".css";
+                    if($(this).attr("href") !== stylesheetHref) {
+                        $(this).remove();
+                    }
+                })
             }
         })
         return this;
